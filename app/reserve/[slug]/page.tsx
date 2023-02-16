346f66401853e28cd163e4a959c45f805667fbd7
denny <denny@playmore.com.hk>
2023-02-16 15:41:54 +0800

    refactor to a layout

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -5,17 +5,12 @@ import Header from './components/Header';
 
 export default function Reserve() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <div className="border-t h-screen">
-          <div className="py-9 w-3/5 m-auto">
-            <Header />
+    <div className="border-t h-screen">
+      <div className="py-9 w-3/5 m-auto">
+        <Header />
 
-            <Form />
-          </div>
-        </div>
-      </main>
-    </main>
+        <Form />
+      </div>
+    </div>
   );
 }
